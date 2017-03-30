8bd4c0376f59dcf6bce7af09f7a70b5a3dddfaf6
Sami Farhat <sami.kfarhat@gmail.com>
2017-03-30 08:26:09 +0100

    add test for AgentIsDeadException

---
+++ src/test/java/AgentIsDeadExceptionTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sami on 30/03/2017.
+ */
+public class AgentIsDeadExceptionTest {
+    @Test
+    public void testDefaultException() {
+        try {
+            throw new AgentIsDeadException();
+        }
+        catch (AgentIsDeadException exc) {
+            assertTrue(true); // pass
+        }
+    }
+
+    @Test
+    public void testExceptionWithMessage() {
+        final String message = Utils.randomString();
+        try {
+            throw new AgentIsDeadException(message);
+        }
+        catch (AgentIsDeadException exc) {
+            assertEquals(message, exc.getMessage());
+        }
+    }
+}
