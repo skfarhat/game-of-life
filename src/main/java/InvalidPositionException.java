14bfea5958f5926613254dcf2abe7fd2b3ca31c0
Sami Farhat <sami.kfarhat@gmail.com>
2017-03-31 06:46:03 +0100

    create InvalidPositionException with test - not used yet

---
+++ src/main/java/InvalidPositionException.java
@@ -0,0 +1,12 @@
+
+/**
+ * Created by Sami on 31/03/2017.
+ */
+public class InvalidPositionException extends Exception {
+
+    /** @brief default constructor */
+    public InvalidPositionException() {super(); };
+
+    /** @brief constructor with String message */
+    public InvalidPositionException(String e) { super(e); };
+}
