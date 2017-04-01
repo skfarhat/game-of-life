74e8c213dae110bdddb998dff33c30c633515d74
Sami Farhat <sami.kfarhat@gmail.com>
2017-04-01 15:40:58 +0100

    create GridCellFactory + test

---
+++ src/main/java/GridCellFactory.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Sami on 01/04/2017.
+ */
+
+/**
+ * @class Factory class to construct a Grid of Cells
+ */
+public class GridCellFactory {
+
+    /** @brief private constructor */
+    private GridCellFactory() {};
+
+    /**
+     * @param rows
+     * @param cols
+     * @return null on failure
+     */
+    public static Grid<Cell> createGridCell(int rows, int cols) throws GridCreationException {
+        Grid<Cell> grid = new Grid<Cell>(Cell.class, rows, cols);
+        return grid;
+    }
+}
---
+++ src/test/java/GridCellFactoryTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sami on 01/04/2017.
+ */
+public class GridCellFactoryTest {
+
+    /**
+     * @brief a hack to call the private constructor
+     * mostly for code coverage completeness
+     */
+    @Test
+    public void testConstructorIsPrivate() throws Exception {
+        Constructor constructor = GridCellFactory.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
+    }
+
+    @Test
+    public void testCrateGridCell() throws GridCreationException {
+        Random rand = new Random(System.currentTimeMillis());
+        final int bound = 100;
+        final int rows = rand.nextInt(bound);
+        final int cols = rand.nextInt(bound);
+
+        // if no exception is thrown we consider the test to have succeeded
+        GridCellFactory.createGridCell(rows, cols);
+    }
+
+}
