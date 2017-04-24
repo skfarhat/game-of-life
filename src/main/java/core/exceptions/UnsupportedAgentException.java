83a52b8b39fcc1325cc1641187dfb0e4f3d299c8
Sami Farhat <sami.kfarhat@gmail.com>
2017-04-24 20:02:52 +0100

    UnsupportedAgentException

---
+++ src/main/java/core/exceptions/UnsupportedAgentException.java
@@ -0,0 +1,16 @@
+package core.exceptions;
+
+import core.Agent;
+
+public class UnsupportedAgentException extends RuntimeException {
+
+    public UnsupportedAgentException() {}
+
+    public UnsupportedAgentException(Class<?extends Agent> cls) {
+        this(String.format("Agent %s type not supported!", cls.getName()));
+    }
+
+    public UnsupportedAgentException(String message) {
+        super(message);
+    }
+}
